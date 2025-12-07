e2cb2e95f2c1d02155546f5d57df49ab2212948e
scto <tschmid35@gmail.com>
2025-12-07 07:32:58 +0100

    fix*

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
@@ -18,18 +18,21 @@ dependencyResolutionManagement {
 rootProject.name = "CodeLikeBastiMove"
 
 include(":app")
-include(":features")
-include(":features:feature-home")
-include(":features:feature-gallery")
-include(":features:feature-slideshow")
-include(":features:feature-settings")
-include(":features:feature-treeview")
-include(":features:feature-editor")
-include(":features:feature-git")
-include(":features:feature-onboarding")
-include(":core:core-datastore-proto")
+
 include(":core:core-datastore")
-include(":core:core-ui")
+include(":core:core-datastore-proto")
 include(":core:core-resources")
 include(":core:templates-api")
 include(":core:templates-impl")
+include(":core:core-ui")
+
+include(":features")
+include(":features:feature-editor")
+//include(":features:feature-gallery")
+include(":features:feature-git")
+include(":features:feature-home")
+include(":features:feature-onboarding")
+include(":features:feature-settings")
+//include(":features:feature-slideshow")
+include(":features:feature-treeview")
+
