8b36217f02efd3f1a1db341a03ffd5065424b03e
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-12-10 20:11:02 +0100

    Modify included features in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,23 +22,22 @@ rootProject.name = "CodeLikeBastiMove"
 
 include(":app")
 
-include(":core:core-logger")
 include(":core:core-datastore")
 include(":core:core-datastore-proto")
+include(":core:core-logger")
 include(":core:core-resources")
 include(":core:templates-api")
 include(":core:templates-impl")
 include(":core:core-ui")
 
 include(":features")
+include(":features:feature-designer")
 include(":features:feature-editor")
 include(":features:feature-explorer")
 include(":features:feature-git")
 include(":features:feature-home")
+include(":features:feature-main")
 include(":features:feature-onboarding")
 include(":features:feature-settings")
-include(":features:feature-treeview")
-include(":features:feature-main")
-include(":features:feature-designer")
 include(":features:feature-slidingpanel")
-
+include(":features:feature-treeview")
