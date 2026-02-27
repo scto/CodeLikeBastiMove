50c7893bdf877b1065ddba82a28ac368de08f5b5
scto <tschmid35@gmail.com>
2026-02-27 20:39:27 +0100

    feat: update settings

--- app/src/main/kotlin/com/scto/codelikebastimove/MainActivity.kt
+++ app/src/main/kotlin/com/scto/codelikebastimove/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.scto.codelikebastimove.core.datastore.ThemeMode as DataStoreThemeMode
 import com.scto.codelikebastimove.core.datastore.UserPreferences
 import com.scto.codelikebastimove.core.datastore.UserPreferencesRepository
+import com.scto.codelikebastimove.core.resources
 import com.scto.codelikebastimove.core.ui.theme.AppTheme
 import com.scto.codelikebastimove.core.ui.theme.ThemeMode
 import com.scto.codelikebastimove.feature.main.MainScreen
